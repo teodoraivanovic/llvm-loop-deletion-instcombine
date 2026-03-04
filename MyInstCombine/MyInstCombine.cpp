ab3e9c4ef77136c523abb247234a2e194c8462bc
realchop <jovanovic.lazar002@gmail.com>
2026-03-04 23:15:34 +0100

    feat: Add arithmetic optimization

--- MyInstCombine/MyInstCombine.cpp
+++ MyInstCombine/MyInstCombine.cpp
@@ -1,14 +1,138 @@
+#include "llvm/IR/PatternMatch.h"
 #include "llvm/Pass.h"
 #include "llvm/Passes/PassBuilder.h"
 #include "llvm/Passes/PassPlugin.h"
 
+#include <algorithm>
+#include <vector>
+
 using namespace llvm;
+using namespace llvm::PatternMatch;
 
 namespace {
 
 struct MyInstCombine : public PassInfoMixin<MyInstCombine> {
 
-    bool run(Function &F) { return false; }
+    static bool replace_instruction(Instruction &I, Value *V) {
+        if (V == &I)
+            return false;
+
+        I.replaceAllUsesWith(V);
+        I.eraseFromParent();
+
+        return true;
+    }
+
+    static bool arithmetic(Instruction &I) {
+        auto *BO = dyn_cast<BinaryOperator>(&I);
+        if (!BO)
+            return false;
+        Value *X = nullptr;
+
+        // X + 0 = 0 + X = X
+        if (match(BO, m_Add(m_Value(X), m_Zero())) ||
+            match(BO, m_Add(m_Zero(), m_Value(X))))
+            return replace_instruction(I, X);
+
+        // X - 0 = X
+        if (match(BO, m_Sub(m_Value(X), m_Zero())))
+            return replace_instruction(I, X);
+
+        // X * 1 = 1 * X = X
+        if (match(BO, m_Mul(m_Value(X), m_One())) ||
+            match(BO, m_Mul(m_One(), m_Value(X))))
+            return replace_instruction(I, X);
+
+        // X * 0 = 0 * X = 0
+        if (match(BO, m_Mul(m_Value(X), m_Zero())) ||
+            match(BO, m_Mul(m_Zero(), m_Value(X))))
+            return replace_instruction(I, Constant::getNullValue(I.getType()));
+
+        // X && 1....1 = 1...1 && X = X
+        if (match(BO, m_And(m_Value(X), m_AllOnes())) ||
+            match(BO, m_And(m_AllOnes(), m_Value(X))))
+            return replace_instruction(I, X);
+
+        // X || 0 = 0 || X = X
+        if (match(BO, m_Or(m_Value(X), m_Zero())) ||
+            match(BO, m_Or(m_Zero(), m_Value(X))))
+            return replace_instruction(I, X);
+
+        // X xor 0 = X
+        if (match(BO, m_Xor(m_Value(X), m_Zero())) ||
+            match(BO, m_Xor(m_Zero(), m_Value(X))))
+            return replace_instruction(I, X);
+
+        // X xor X = X - X = 0
+        if (match(BO, m_Xor(m_Value(X), m_Specific(X))) ||
+            match(BO, m_Sub(m_Value(X), m_Specific(X))))
+            return replace_instruction(I, Constant::getNullValue(I.getType()));
+
+        return false;
+    }
+
+    bool apply(Instruction &I) {
+        auto optimizations = {arithmetic};
+
+        for (auto &O : optimizations) {
+            if (O(I))
+                return true;
+        }
+
+        return false;
+    }
+
+    bool run(Function &F) {
+        bool changed = false;
+        bool local_changed = true;
+        unsigned iteration = 0;
+        const unsigned MAX_ITERATIONS = 100;
+
+        while (local_changed && iteration < MAX_ITERATIONS) {
+            local_changed = false;
+            ++iteration;
+
+            std::vector<Instruction *> instructions;
+            for (BasicBlock &BB : F) {
+                for (Instruction &I : BB) {
+                    instructions.push_back(&I);
+                }
+            }
+
+            for (unsigned i = 0; i < instructions.size(); ++i) {
+                Instruction *I = instructions[i];
+
+                if (!I || !I->getParent())
+                    continue;
+
+                std::vector<Instruction *> users;
+                for (User *U : I->users()) {
+                    if (auto *UI = dyn_cast<Instruction>(U)) {
+                        users.push_back(UI);
+                    }
+                }
+
+                bool changed = apply(*I);
+                local_changed |= changed;
+
+                if (changed) {
+                    for (Instruction *UI : users) {
+                        if (UI->getParent()) {
+                            if (std::find(instructions.begin() + i + 1,
+                                          instructions.end(),
+                                          UI) == instructions.end()) {
+                                instructions.push_back(UI);
+                            }
+                        }
+                    }
+                }
+            }
+
+            changed |= local_changed;
+        }
+
+        return changed;
+    }
 
     PreservedAnalyses run(Function &F, FunctionAnalysisManager &AM) {
         bool changed = run(F);
